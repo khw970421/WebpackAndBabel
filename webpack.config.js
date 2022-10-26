dc7486a72d7ee38afaeebe9b44cea48efc83ca83
khw970421 <khw970421@kakao.com>
2022-10-26 15:44:40 +0900

    ✨ feat : babel없는 Webpack 사용

--- webpack.config.js
+++ webpack.config.js
@@ -11,31 +11,5 @@ module.exports = {
     filename: "[name].js",
     path: `${__dirname}/dist`,
   },
-  mode: "production",
-  module: {
-    rules: [
-      {
-        include: [path.resolve(__dirname, "src/js")],
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset"],
-            plugins: ["@babel/plugin"],
-          },
-        },
-      },
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_module/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+  mode: "production"
 };
