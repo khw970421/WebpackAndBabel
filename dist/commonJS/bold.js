aa6e47efece4a50ebb6477c37dce2f249b301054
khw970421 <khw970421@kakao.com>
2022-10-28 14:57:18 +0900

    🐛 fix : 중복 코드 제거

--- dist/commonJS/bold.js
+++ dist/commonJS/bold.js
@@ -3,7 +3,3 @@ module.exports = function (name) {
     return "<b>" + name + "</b>";
 };
 module.exports.boldTagName = "b";
-module.exports = function (name) {
-    return "<b>" + name + "</b>";
-};
-module.exports.boldTagName = "b";
