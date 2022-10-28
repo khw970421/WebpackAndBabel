e7d3245f6c98fd9f5005d78318de6125e0d99564
khw970421 <khw970421@kakao.com>
2022-10-28 13:43:18 +0900

    ✨ feat : ES6와 commonJS 컴파일 결과 분리

---
+++ dist/ES6/index.js
@@ -0,0 +1,13 @@
+import bold, { boldTagName } from "./bold";
+import italic, { italicTagName } from "./italic";
+let isBold = true;
+export function sayHello(name) {
+    const formatter = isBold ? bold : italic;
+    isBold = !isBold;
+    return `Hello! ${formatter(name)}!`;
+}
+console.log(sayHello("정환")); // 'Hello! <b>정환</b>!'
+console.log(sayHello("정환")); // 'Hello! <i>정환</i>!'
+console.log(boldTagName); // 'b'
+console.log(italicTagName); // 'i'
+[1, 2, 3].map((n) => n + 1);
